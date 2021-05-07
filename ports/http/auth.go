8ac71ba78335ab453b44bfaf5b4d172b58bf8cc8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-07 22:30:10 +0100

    SignUp implemented

--- ports/http/auth.go
+++ ports/http/auth.go
@@ -1,11 +1,40 @@
 package http
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"lohon.cm/msvc/auth/db"
 )
 
-func (hs *HttpServer) SignUp(w http.ResponseWriter, r *http.Request) {
+func (h *HttpServer) SignUp(w http.ResponseWriter, r *http.Request) {
+	var user *db.User
 
+	if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+		JSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	foundUser, err := h.DB.FindUserByEmail(user.Email)
+	if err != nil {
+		JSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if foundUser != nil {
+		JSON(w, http.StatusBadRequest, "user with this email already exists")
+		return
+	}
+
+	if err := h.DB.SaveUser(user); err != nil {
+		JSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// TODO: Send message through the broker annoucing the user creation
+	// TODO: Send email - User account Verification
+
+	JSON(w, http.StatusOK, true)
 }
 
 func (hs *HttpServer) SignIn(w http.ResponseWriter, r *http.Request) {
--- ports/http/http.go
+++ ports/http/http.go
@@ -1,7 +1,22 @@
 package http
 
-import "lohon.cm/msvc/auth/db"
+import (
+	"encoding/json"
+	"net/http"
+
+	"lohon.cm/msvc/auth/db"
+)
 
 type HttpServer struct {
 	DB *db.DBQuery
+}
+
+func JSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
