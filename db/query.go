f4a629bc6c280f371f215690088cb2f3ba6f33d5
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-07 22:08:48 +0100

    Implementing SaveUser and FindUserByEmail

--- db/query.go
+++ db/query.go
@@ -1,6 +1,13 @@
 package db
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Query interface {
 	FindUserByEmail(email string)
@@ -22,8 +29,36 @@ func NewQuery(database *mongo.Database) *DBQuery {
 	}
 }
 
-func (db DBQuery) FindUserByEmail(email string) {}
-func (db DBQuery) SaveUser()                    {}
+func (db DBQuery) FindUserByEmail(email string) (*User, error) {
+	var user User
+
+	if err := db.database.Collection("users").FindOne(context.Background(), bson.M{"email": email}).Decode(&user); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+func (db DBQuery) SaveUser(user *User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	var usersColl = db.database.Collection("users")
+	user.Password = string(hash)
+	insertResult, err := usersColl.InsertOne(context.Background(), user)
+	if err != nil {
+		return err
+	}
+
+	user.ID = insertResult.InsertedID.(primitive.ObjectID)
+	return nil
+}
 
 func (db DBQuery) DeleteResetPassword(email string)           {}
 func (db DBQuery) SaveResetPasswordToken(email, token string) {}
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module lohon.cm/msvc/auth
 go 1.16
 
 require (
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/go-hclog v0.16.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.2 // indirect
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
 )
